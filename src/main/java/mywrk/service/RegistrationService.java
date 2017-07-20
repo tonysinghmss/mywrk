5b882df99364e090b72020794dc0929144ce9f9a
tonysinghmss <manishankarsngh007@gmail.com>
2017-07-21 00:34:06 +0530

    User registration

--- src/main/java/mywrk/service/RegistrationService.java
+++ src/main/java/mywrk/service/RegistrationService.java
@@ -3,16 +3,25 @@ package mywrk.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import mywrk.dao.contracts.RegistrationRepo;
+import mywrk.domain.Login;
 import mywrk.domain.User;
+import mywrk.security.SecurityUtils;
 @Service
 public class RegistrationService implements Registration {
 	
+	@Autowired private RegistrationRepo registrationRepo;
 	
 	@Override
 	public boolean registerUser(User usr) {
-		// TODO Generate Login object, Users object
-		// TODO Login should have password hash and salt
-		return false;
+		String pwdHash = SecurityUtils.getPasswordHash(usr.getPassword());
+		String pwdSalt = SecurityUtils.getPasswordSalt(usr.getPassword());
+		Login login = Login.newinstance()
+				.withUserName(usr.getUserName())
+				.withPasswordSalt(pwdSalt)
+				.withPasswordHash(pwdHash)
+				.build();
+		return registrationRepo.registerUser(usr, login);
 	}
 
 }
