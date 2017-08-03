8878e22083666a8ecc7284d0519fff8287fd65e0
tonysinghmss <manishankarsngh007@gmail.com>
2017-08-03 21:22:59 +0530

    New entities added

--- src/main/resources/Datamodel.sql
+++ src/main/resources/Datamodel.sql
@@ -73,3 +73,122 @@ WITH (
 ALTER TABLE mywrk.users
   OWNER TO tony;
 
+CREATE TABLE mywrk.invoicelineitems
+(
+    invoicelineitem_id bigint,
+    related_invoice_id bigint,
+    price numeric(10, 2)
+)
+WITH (
+    OIDS = FALSE
+)
+;
+
+ALTER TABLE mywrk.invoicelineitems
+    OWNER to tony;
+    
+CREATE TABLE mywrk.invoices
+(
+    invoice_id bigint,
+    description text
+)
+WITH (
+    OIDS = FALSE
+)
+;
+
+ALTER TABLE mywrk.invoices
+    OWNER to tony;
+    
+CREATE TABLE mywrk.orderlineitems
+(
+    orderlineitem_id bigint,
+    related_order_id bigint,
+    related_work_id bigint,
+    order_number integer,
+    invoicelineitem_id bigint,
+    paid boolean
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE mywrk.orderlineitems
+    OWNER to tony;
+
+CREATE TABLE mywrk.orders
+(
+    order_id bigint,
+    related_user_id bigint,
+    order_dttm timestamp without time zone,
+    required_date timestamp without time zone,
+    fulfilled boolean,
+    paid boolean
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE mywrk.orders
+    OWNER to tony;
+    
+CREATE TABLE mywrk.payments
+(
+    payment_id bigint,
+    related_user_id bigint,
+    related_invoice_id bigint,
+    amount_paid numeric(10, 2),
+    paid_on timestamp without time zone
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE mywrk.payments
+    OWNER to tony;
+    
+CREATE TABLE mywrk.work_category
+(
+    work_category_id bigint,
+    category_name text COLLATE "default".pg_catalog,
+    description text COLLATE "default".pg_catalog,
+    created_by bigint,
+    creation_dttm timestamp without time zone
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE mywrk.work_category
+    OWNER to tony;
+   
+ 
+CREATE TABLE mywrk.work_status
+(
+    work_status_id bigint,
+    fulfilled boolean,
+    last_worked_on timestamp without time zone
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE mywrk.work_status
+    OWNER to tony;
+    
+
+CREATE TABLE mywrk.works
+(
+    work_id bigint,
+    related_work_category_id bigint,
+    related_work_status_id bigint,
+    work_created_on timestamp without time zone
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE mywrk.works
+    OWNER to tony;
